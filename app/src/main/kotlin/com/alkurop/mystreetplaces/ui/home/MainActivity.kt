ca235beda5b9be77fcb67f0f3bcd6b9c19d755a7
Alex Kurop <alkurop@gmail.com>
2017-07-03 11:17:37 +0300

    Add login activity

---
+++ app/src/main/kotlin/com/alkurop/mystreetplaces/ui/home/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.alkurop.mystreetplaces.ui.home
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.alkurop.mystreetplaces.R
+
+
+class MainActivity : AppCompatActivity(){
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
--- app/src/main/kotlin/com/alkurop/mystreetplaces/ui/login/LoginActivity.kt
+++ app/src/main/kotlin/com/alkurop/mystreetplaces/ui/login/LoginActivity.kt
@@ -1,13 +1,13 @@
-package com.alkurop.mystreetplaces.ui
+package com.alkurop.mystreetplaces.ui.login
 
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
 import com.alkurop.mystreetplaces.R
 
+class LoginActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity(){
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(R.layout.activity_login)
   }
 }
