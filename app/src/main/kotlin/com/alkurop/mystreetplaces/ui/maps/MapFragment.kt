dda0ccd1279a681d609954d35e4490185955d35c
alkurop <alkurop@gmail.com>
2017-07-18 07:13:15 +0200

    Add map view

--- app/src/main/kotlin/com/alkurop/mystreetplaces/ui/maps/MapFragment.kt
+++ app/src/main/kotlin/com/alkurop/mystreetplaces/ui/maps/MapFragment.kt
@@ -1,9 +1,14 @@
 package com.alkurop.mystreetplaces.ui.maps
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.alkurop.mystreetplaces.R
 import com.alkurop.mystreetplaces.ui.base.BaseMvpFragment
 import com.alkurop.mystreetplaces.ui.navigation.NavigationAction
 import io.reactivex.Observable
+import kotlinx.android.synthetic.main.fragment_map.*
 import javax.inject.Inject
 
 
@@ -16,6 +21,44 @@ class MapFragment : BaseMvpFragment<MapViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         component().inject(this)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mapView.onDestroy()
     }
 
     override fun renderView(viewModel: MapViewModel) {
