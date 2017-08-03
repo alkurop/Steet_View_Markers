5c19f89fa7e110331f573a4b946541f245a69357
alkurop <alkurop@gmail.com>
2017-08-03 13:41:55 +0200

    Pictures adapter

--- app/src/main/kotlin/com/alkurop/mystreetplaces/ui/pin/pictures/PicturesAdapter.kt
+++ app/src/main/kotlin/com/alkurop/mystreetplaces/ui/pin/pictures/PicturesAdapter.kt
@@ -1,5 +1,6 @@
 package com.alkurop.mystreetplaces.ui.pin.pictures
 
+import android.graphics.Bitmap
 import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -7,6 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.alkurop.mystreetplaces.R
 import com.alkurop.mystreetplaces.data.pin.PictureWrapper
+import com.nostra13.universalimageloader.core.ImageLoader
+import com.nostra13.universalimageloader.core.assist.FailReason
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener
+import kotlinx.android.synthetic.main.item_add_picture.view.*
+import kotlinx.android.synthetic.main.item_existing_picture.view.*
 
 class PicturesAdapter : RecyclerView.Adapter<PicturesAdapter.PictureVH>() {
     private var pictures = listOf<PictureWrapper>()
@@ -61,13 +68,45 @@ class PicturesAdapter : RecyclerView.Adapter<PicturesAdapter.PictureVH>() {
         return position
     }
 
-    abstract class PictureVH(itemView: View?) : RecyclerView.ViewHolder(itemView)
+    abstract class PictureVH(view: View?) : RecyclerView.ViewHolder(view)
 
-    class AddPictureVh(itemView: View?) : PictureVH(itemView)
+    inner class AddPictureVh(view: View?) : PictureVH(view) {
+        init {
+            itemView.addContainer.setOnClickListener {
+                onAddPictureClick?.invoke()
+            }
+        }
+    }
 
-    class ExistingPictureVh(itemView: View?) : PictureVH(itemView) {
+    inner class ExistingPictureVh(view: View?) : PictureVH(view) {
+        init {
+            itemView.pictureContainer.setOnClickListener {
+                onPictureClick?.invoke(getItem(adapterPosition))
+            }
+        }
+
         fun bind(item: PictureWrapper) {
+            val imagePath = item.localPhoto ?: item.serverPhoto
+            ImageLoader.getInstance().displayImage(imagePath, itemView.photoView, object : SimpleImageLoadingListener() {
+                override fun onLoadingStarted(imageUri: String?, view: View?) {
+                    itemView.progress_bar.visibility = View.VISIBLE
+                }
 
+                override fun onLoadingComplete(imageUri: String?, view: View?, loadedImage: Bitmap?) {
+                    itemView.progress_bar.visibility = View.GONE
+                }
+
+                override fun onLoadingFailed(imageUri: String?, view: View?, failReason: FailReason?) {
+                    itemView.progress_bar.visibility = View.GONE
+                }
+            })
+        }
+    }
+
+    override fun onViewRecycled(holder: PictureVH?) {
+        super.onViewRecycled(holder)
+        if (holder is ExistingPictureVh) {
+            ImageLoader.getInstance().cancelDisplayTask(holder.itemView.photoView)
         }
     }
 }
